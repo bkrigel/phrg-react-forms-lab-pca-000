e0a444b8750da055ad023a116e9d4f5769d9ee73
Benjamin Krigel <benjamin.krigel@gmail.com>
2018-10-03 16:52:18 -0400

    I think I could still use some practice with these forms and form validators

--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -1,19 +1,46 @@
 import React from "react";
 
+const isValidPoem = (poem) => {
+  const poemSplit = poem.split("\n").map(line => line.trim())
+  const isThreeLines = poemSplit.length === 3
+  if (poem && isThreeLines) {
+    return (
+      poemSplit[0].split(" ").length === 5 &&
+      poemSplit[1].split(" ").length === 3 &&
+      poemSplit[2].split(" ").length === 5
+    )
+  } else {
+    return false
+  }
+}
+
 class PoemWriter extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      content: "",
+      isValid: true
+    };
+  }
+
+  setPoemContent = (event) => {
+    const content = event.target.value
+    this.setState({
+      content,
+      isValid: isValidPoem(content)
+    })
   }
 
   render() {
     return (
       <div>
-        <textarea rows="3" cols="60" />
-        <div id="poem-validation-error" style={{ color: "red" }}>
-          This poem is not written in the right structure!
-        </div>
+        <textarea rows="3" cols="60" value={this.state.content} onChange={this.setPoemContent}/>
+        {!this.state.isValid ? (
+          <div id="poem-validation-error" style={{ color: "red" }}>
+            This poem is not written in the right structure!
+          </div>
+        ) : null}
       </div>
     );
   }
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,20 +1,39 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      tweet: ""
+    };
+  }
+
+  handleChange = event => {
+    this.setState({
+      tweet: event.target.value
+    });
   }
 
   render() {
     return (
       <div>
-        <strong>Your message:</strong>
-        <input type="text" />
+        <strong>Your message: </strong>
+        <input type="text" value={this.state.tweet} onChange={this.handleChange} />
+        <br/>
+        <small>Characters Remaining: {this.props.maxChars - this.state.tweet.length}</small>
       </div>
     );
   }
+}
+
+TwitterMessage.propTypes = {
+  maxChars: PropTypes.number
+}
+
+TwitterMessage.defaultProps = {
+  maxChars: 140
 }
 
 export default TwitterMessage;
